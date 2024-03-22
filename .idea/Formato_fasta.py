420d7751aa2f63e00d87deba65408f3a483d1948
JonatnV <116097178+JonatnV@users.noreply.github.com>
2024-03-22 18:58:15 -0500

    Ejercicio1
    Se realizo el ejercicio de extraer del archivo todas las secuencias y almacenarlas en una lista de clases fastas que guardan el id, la longitud y la secuencia que corresponde a cada una,

--- .idea/Formato_fasta.py
+++ .idea/Formato_fasta.py
@@ -2,15 +2,35 @@ import os
 arch= os.path.join(os.getcwd())
 arch+="/data/xylefa8416.fasta"
 class fasta():
-    def __init__(self,name,lon,sec):
-        self.nombre=name
-        self.longitud=lon
+    def __init__(self,nom,length,sec):
+        self.nombre=nom
+        self.longitud=length
         self.secuencia=sec
-
+    def __str__(self):
+        return self.nombre+" len="+str(self.longitud)+"\n"+self.secuencia
 with open(arch,"r") as file:
     all=file.read()
     #print(all)
     Fastas=[]
     seccionado=all.split(">")
     num_fastas=len(seccionado)
-    print(seccionado[128])
+    for i in range(1,len(seccionado)):
+        Posiciones=[]
+        Posiciones.append(seccionado[i].find("T"))
+        Posiciones.append(seccionado[i].find("G"))
+        Posiciones.append(seccionado[i].find("A"))
+        Posiciones.append(seccionado[i].find("C"))
+        Posiciones.sort()
+        HEAD=seccionado[i][0:Posiciones[0]]
+        ID=HEAD[0:9]
+        LEN=HEAD[14:]
+        SECUENCIA=seccionado[i][Posiciones[0]:]
+        nuevaFasta= fasta(ID,LEN,SECUENCIA)
+        Fastas.append(nuevaFasta)
+
+        Posiciones.clear()
+
+    print(seccionado[127][0:18])
+    print(len(Fastas))
+    print(Fastas[127].nombre)
+
